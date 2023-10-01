4179b727c495e2298fe94a1ca887d6d39c89ef5e
Zoheb Ahmed <86772325+zohebahmed08@users.noreply.github.com>
2023-10-01 13:20:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import skew, kurtosis
+
+# Load the data from the specified file path
+file_path = r'E:\COLLEGE CODES\archive\Healthcare-Diabetes.csv'
+data = pd.read_csv(file_path)
+
+# Data Cleaning (you can customize this based on your dataset)
+# For example, you can remove missing values or handle outliers here.
+
+# Calculate Mean
+mean = data.mean()
+
+# Calculate Median
+median = data.median()
+
+# Calculate Standard Deviation
+std_dev = data.std()
+
+# Calculate Skewness
+data_skewness = skew(data, axis=0)
+
+# Calculate Kurtosis
+data_kurtosis = kurtosis(data, axis=0)
+
+# Create a DataFrame to store the results
+summary_stats = pd.DataFrame({
+    'Mean': mean,
+    'Median': median,
+    'Standard Deviation': std_dev,
+    'Skewness': data_skewness,
+    'Kurtosis': data_kurtosis
+})
+
+# Display the summary statistics
+print(summary_stats)
